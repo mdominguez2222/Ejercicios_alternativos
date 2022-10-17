d228f55ada8d173f80cd042f485ff479001d6b54
Minerva <al_mpacod@iessierradelasvillas.org>
2022-10-17 11:15:57 +0200

    Cambio ejercicio Agendas de Teléfonos

--- Act9.py
+++ Act9.py
@@ -6,9 +6,13 @@ num1 = int(input("Dime un número: \n"))
 num2 = int(input("Dime otro número: \n"))
 num3 = int(input("Dime otro número: \n"))
 
-vNumeros.append(num1)
+vNumeros.append(num1)          #o así: vNumeros.append(int(input("Dime un número: \n"))) 
 vNumeros.append(num2)
 vNumeros.append(num3)
 vNumeros.sort(reverse=True)
 
 print (vNumeros)
+print("El mayor es", vNumeros[0])  #o así: print(max(vNumeros))
+
+ultimo = len(vNumeros)
+print("El menor es", vNumeros[ultimo-1])
